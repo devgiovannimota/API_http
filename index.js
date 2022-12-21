b22e0666bacbbc6cd2c83f9202c416d5d7d4233e
Giovanni Mota <devgiovannimota@gmail.com>
2022-12-21 13:07:36 -0300

    Final commit

--- index.js
+++ index.js
@@ -1,9 +1,48 @@
 const http = require("http");
+const { randomUUID } = require("crypto");
+const { on } = require("events");
 
+const users = [];
 const server = http.createServer((request, response) => {
   if (request.url === "/users") {
     if (request.method === "GET") {
-      return response.end("Aplicação está no ar");
+      return response.end(JSON.stringify(users));
+    }
+    if (request.method === "POST") {
+      request
+        .on("data", (data) => {
+          const dataUser = JSON.parse(data);
+
+          const user = {
+            id: randomUUID(),
+            ...dataUser,
+          };
+
+          users.push(user);
+        })
+        .on("end", () => {
+          return response.end("Aplicação POST feita");
+        });
+    }
+  }
+  if (request.url.startsWith("/users")) {
+    if (request.method === "PUT") {
+      const url = request.url;
+      const splitURL = url.split("/");
+      const idUser = splitURL[2];
+      const userIndex = users.findIndex((user) => user.id === idUser);
+
+      request
+        .on("data", (data) => {
+          const dataUser = JSON.parse(data);
+          users[userIndex] = {
+            id: idUser,
+            ...dataUser,
+          };
+        })
+        .on("end", () => {
+          return response.end(JSON.stringify(users[userIndex]));
+        });
     }
   }
 });
